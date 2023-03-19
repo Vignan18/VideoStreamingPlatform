d9c490a77a0f32b16f9ffe0e7051a196fdc3ec45
Vignan <35171316+Vignan18@users.noreply.github.com>
2023-03-19 18:36:16 +0530

    search redirection

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
 import { useDispatch,useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { toggleMenu } from "../utils/appSlice";
 import {YOUTUBE_SEARCH_API} from "../utils/constants";
 import { cacheResults,searchQueryfn } from "../utils/searchSlice";
@@ -82,7 +83,6 @@ const Head = () => {
           className="h-8 hover:cursor-pointer"
           alt="menu"
           onClick={() => toggleMenuHandler()}
-          
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
         />
 
@@ -104,9 +104,11 @@ const Head = () => {
           onFocus={() => setShowSuggestions(true)}
           onBlur={() => setShowSuggestions(false)}
         />
+        <Link to="/">
         <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100" onClick={handleSearch}>
           🔍
-        </button>
+        </button></Link>
+
       </div>
       {showSuggestions && (
           <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 const API_KEY = "AIzaSyDL7CxvCE0w2M6duBmGM4-TabZczqpUb78"
 export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+API_KEY;
 export const YOUTUBE_SEARCH_API =
-"http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const LIVE_CHAT_COUNT = 25;
 export const GET_COMMENTS_API =
     "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=60&key=" +
